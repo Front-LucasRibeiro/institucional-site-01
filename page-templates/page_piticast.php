6db2075e94e387c50e3b97e8a46a6c5f809eca11
Lucas Ribeiro <lucas.ribeiro@driven.cx>
2021-05-24 13:50:53 -0300

    teste de mudança na pagina de piticast

--- page-templates/page_piticast.php
+++ page-templates/page_piticast.php
@@ -11,6 +11,9 @@ $sa_settings = get_option( 'sa_options', $sa_options );
 <div class="institucional page piticast">  
   <div class="container">
     <div id='buzzsprout-large-player-1757249'></div>
+    <!-- <div id='buzzsprout-small-player-1757249'></div><script type='text/javascript' charset='utf-8' src='https://www.buzzsprout.com/1757249.js?container_id=buzzsprout-small-player-1757249&player=small'></script> -->
+    <!-- <iframe src="https://www.buzzsprout.com/1757249" title="PitiCast - Podcast dedicado aos amantes de Cultura Pop"></iframe> -->
+
     <?php the_content(); ?>
   </div>
 </div>
