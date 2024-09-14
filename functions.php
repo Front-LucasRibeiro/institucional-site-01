a87bd40a04cedd81080557805fbbc3ed5c528067
Lucas Maranho <lksribeiro2014@gmail.com>
2024-09-14 15:44:47 -0300

    deixando header administravel

--- functions.php
+++ functions.php
@@ -3,19 +3,36 @@ add_filter('show_admin_bar', '__return_false');
 
 add_theme_support('post-thumbnails');
 
+add_filter('wp_cache_disable', '__return_true');
+
+global $wpdb;
+$wpdb->query( "DELETE FROM $wpdb->options WHERE option_name LIKE '_transient_%'" );
+
+wp_cache_flush();
+
+define('WP_CACHE', false);
+
+function enqueue_media_uploader() {
+    // Verifica se estamos na tela de edição de post
+    if (is_admin()) {
+        wp_enqueue_media(); // Carrega os scripts de mídia
+    }
+}
+add_action('admin_enqueue_scripts', 'enqueue_media_uploader');
+
 // Ocultar abas do painel do WordPress
 function remove_admin_menu_items() {
     remove_menu_page('index.php'); // Painel
     remove_menu_page('edit.php'); // Posts
     remove_menu_page('edit-comments.php'); // Comentários
     remove_menu_page('wpforms-overview'); // WPForms
-    remove_menu_page('plugins.php'); // Plugins
+    // remove_menu_page('plugins.php'); // Plugins
     remove_menu_page('themes.php'); // Aparência
     remove_menu_page('tools.php'); // Ferramentas
 }
 add_action('admin_menu', 'remove_admin_menu_items');
 
-// start - POST TYPES 
+// start - PORTFOLIO 
 // start - criando campos para o novo post type
 function createFieldPortfolio() {
     global $post;
@@ -97,12 +114,12 @@ function create_portfolio_cpt() {
     register_post_type('list-portfolio', $args);
 }
 add_action('init', 'create_portfolio_cpt');
+// end - PORTFOLIO 
 
 
 
 
-
-
+// start - BLOG 
 // start - criando campos para o novo post type
 function createFieldBlog() {
     global $post;
@@ -163,7 +180,7 @@ function save_blog_meta($post_id) {
     }
 
     // Verifica se o post é do tipo correto
-    if (get_post_type($post_id) !== 'list-blog') {
+    if (get_post_type($post_id) !== 'list-menu') {
         return;
     }
 
@@ -193,3 +210,258 @@ function create_blog_cpt() {
     register_post_type('list-blog', $args);
 }
 add_action('init', 'create_blog_cpt');
+// end - BLOG 
+
+
+// start - MENU 
+// start - criando campos para o novo post type
+function createFieldMenu() {
+    global $post;
+    $id_post = $post->ID;
+    $itemServicesInativar = get_post_meta($id_post, 'itemServicesInativar', true);
+    $LinkInativaritemFacebookLink = get_post_meta($id_post, 'LinkInativaritemFacebookLink', true);
+    $itemPortfolioInativar = get_post_meta($id_post, 'itemPortfolioInativar', true);
+    $itemEmpresaInativar = get_post_meta($id_post, 'itemEmpresaInativar', true);
+    $itemBlogInativar = get_post_meta($id_post, 'itemBlogInativar', true);
+    $itemContatoInativar = get_post_meta($id_post, 'itemContatoInativar', true);
+    $itemLogo = get_post_meta($id_post, 'itemLogo', true); 
+    ?>
+
+<style>
+    textarea,
+    input[type="text"],
+    input[type="hidden"] {
+        width: 100%;
+        margin-top: 5px;
+        padding: 8px;
+        border: 1px solid #ddd;
+        border-radius: 4px;
+    }
+
+    input[type="checkbox"] {
+        position: relative;
+        top: 2px;
+    }
+
+    label {
+        font-weight: bold;
+        display: block;
+        margin-bottom: 5px;
+    }
+
+    .field {
+        margin: 12px 0;
+        border-bottom: 1px solid #ddd;
+        padding-bottom: 15px;
+    }
+
+    .field button {
+        margin-top: 5px;
+    }
+
+    .logo-preview {
+        margin-top: 10px;
+    }
+
+    .logo-preview img {
+        max-width: 100px;
+        height: auto;
+        display: block;
+        border: 1px solid #ddd;
+        border-radius: 4px;
+        padding: 4px;
+    }
+
+    .social-field {
+        margin-bottom: 20px;
+    }
+</style>
+
+<div class="box">
+    <!-- Campo de Upload de Logo -->
+    <div class="field">
+        <label for="itemLogo">Logo:</label>
+        <input type="hidden" id="itemLogo" name="itemLogo" value="<?php echo esc_attr($itemLogo); ?>" />
+        <button type="button" class="button" id="uploadLogoButton">Selecionar Logo</button>
+
+        <div class="logo-preview">
+            <?php if ($itemLogo) : ?>
+                <img src="<?php echo wp_get_attachment_url($itemLogo); ?>" alt="Logo" />
+            <?php endif; ?>
+        </div>
+    </div>
+    <br>
+                 
+    <script>
+        jQuery(document).ready(function($) {
+            var mediaUploader;
+
+            $('#uploadLogoButton').click(function(e) {
+                e.preventDefault();
+                if (mediaUploader) {
+                    mediaUploader.open();
+                    return;
+                }
+
+                mediaUploader = wp.media.frames.file_frame = wp.media({
+                    title: 'Escolher Logo',
+                    button: {
+                        text: 'Escolher Logo'
+                    },
+                    multiple: false
+                });
+
+                mediaUploader.on('select', function() {
+                    var attachment = mediaUploader.state().get('selection').first().toJSON();
+                    $('#itemLogo').val(attachment.id);
+                    $('.logo-preview').html('<img src="' + attachment.url + '" alt="Logo" />');
+                });
+
+                mediaUploader.open();
+            });
+        });
+    </script>
+
+    <!-- Campos de Inativar -->
+    <?php
+    $fields = [
+        'Serviços' => 'itemServicesInativar',
+        'Portfólio' => 'itemPortfolioInativar',
+        'Empresa' => 'itemEmpresaInativar',
+        'Blog' => 'itemBlogInativar',
+        'Contato' => 'itemContatoInativar',
+    ];
+
+    foreach ($fields as $label => $name) {
+        $value = get_post_meta($post->ID, $name, true);
+        echo '<div class="field">';
+        echo '<label for="' . esc_attr($name) . '">' . esc_html($label) . ':</label>';
+        echo 'Desativar: <input type="checkbox" id="' . esc_attr($name) . '" name="' . esc_attr($name) . '" value="1" ' . checked($value, '1', false) . ' />';
+        echo '</div>';
+    }
+    ?>
+    <br><br>
+
+    <!-- Redes Sociais -->
+    <?php
+    $socialFields = [
+        'Facebook' => 'itemFacebookLink',
+        'Instagram' => 'itemInstagramLink',
+        'LinkedIn' => 'itemLinkedInLink',
+        'WhatsApp' => 'itemWhatsAppLink',
+    ];
+
+    foreach ($socialFields as $label => $name) {
+        $value = get_post_meta($post->ID, $name, true);
+        $inactivateName = str_replace('Link', 'Inativar', $name);
+        $inactivateValue = get_post_meta($post->ID, $inactivateName, true);
+        ?>
+        <div class="social-field">
+            <label for="<?= esc_attr($name); ?>"><?= esc_html($label); ?>:</label>
+            <input type="text" id="<?= esc_attr($name); ?>" name="<?= esc_attr($name); ?>" value="<?= esc_attr($value); ?>" />
+            <div class="field">
+                Desativar <input type="checkbox" id="<?= esc_attr($inactivateName); ?>" name="<?= esc_attr($inactivateName); ?>" value="1" <?= checked($inactivateValue, '1', false); ?> />
+            </div>
+        </div>
+        <?php
+    }
+    ?>
+</div>
+
+
+
+    <?php
+    // Adicionando nonce para segurança
+    wp_nonce_field('save_menu_meta', 'menu_nonce');
+
+     if (defined('WP_CACHE') && WP_CACHE) {
+        wp_cache_flush();
+    }
+}
+
+function add_menu_meta_box() {
+    add_meta_box(
+        'menu_meta_box', // ID
+        'Informações do Menu', // Título
+        'createFieldMenu', // Callback
+        'list-menu', // Novo CPT
+        'normal', // Contexto
+        'high' // Prioridade
+    );
+}
+add_action('add_meta_boxes', 'add_menu_meta_box');
+
+// end - funções atualizar campos 
+function save_menu_meta($post_id) {
+    // Verifica o nonce
+    if (!isset($_POST['menu_nonce']) || !wp_verify_nonce($_POST['menu_nonce'], 'save_menu_meta')) {
+        return;
+    }
+
+    // Verifica se o post é do tipo correto
+    if (get_post_type($post_id) !== 'list-menu') {
+        return;
+    }
+
+    // Salva o logo
+    if (isset($_POST['itemLogo'])) {
+        update_post_meta($post_id, 'itemLogo', sanitize_text_field($_POST['itemLogo']));
+    }
+
+    // Atualiza os campos de inativar
+    $fields = [
+        'itemServicesInativar',
+        'itemPortfolioInativar',
+        'itemEmpresaInativar',
+        'itemBlogInativar',
+        'itemContatoInativar'
+    ];
+
+    foreach ($fields as $field) {
+        update_post_meta($post_id, $field, isset($_POST[$field]) ? '1' : '0');
+    }
+
+    // Salva os campos de redes sociais
+    $socialFields = [
+        'itemFacebookLink',
+        'itemInstagramLink',
+        'itemLinkedInLink',
+        'itemWhatsAppLink'
+    ];
+
+    foreach ($socialFields as $field) {
+        if (isset($_POST[$field])) {
+            update_post_meta($post_id, $field, sanitize_text_field($_POST[$field]));
+        }
+    }
+
+    // Atualiza os campos de inativar redes sociais
+    foreach ($socialFields as $field) {
+        $inactivateField = str_replace('Link', 'Inativar', $field);
+        update_post_meta($post_id, $inactivateField, isset($_POST[$inactivateField]) ? '1' : '0');
+    }
+
+     if (defined('WP_CACHE') && WP_CACHE) {
+        wp_cache_flush();
+    }
+}
+add_action('save_post', 'save_menu_meta');
+
+
+// Criar o novo post type "Blog"
+function create_menu_cpt() {
+    $args = array(
+        'labels' => array(
+            'name' => __('Menu'),
+            'singular_name' => __('Menu')
+        ),
+        'public' => true,
+        'has_archive' => true,
+        'rewrite' => array('slug' => 'list-menu'),
+        'supports' => array('title'),
+        'menu_icon' => 'dashicons-admin-post',
+    );
+    register_post_type('list-menu', $args);
+}
+add_action('init', 'create_menu_cpt');
+// end - MENU 
--- header.php
+++ header.php
@@ -6,6 +6,12 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<?php $home = get_template_directory_uri(); ?>
+
+	<!-- Adicionando cabeçalhos para prevenir cache -->
+	<meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+	<meta http-equiv="Pragma" content="no-cache" />
+	<meta http-equiv="Expires" content="0" />
+
 	<link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css" />
 	<link rel="stylesheet" href="<?= $home; ?>/src/build/css/index.css" type="text/css" />
 	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
@@ -13,82 +19,142 @@
 
 <body>
 	<header class="header">
-		<div class="container">
-			<a href="/">
-				<div class="logo">
-					<img src="<?= $home; ?>/src/images/logo-3.jpg" alt="">
-				</div>
-			</a>
-			<div class="menu">
-				<ul>
-					<li><a href="/#servicos">Serviços</a></li>
-					<li><a href="/#portfolio">Portfólio</a></li>
-					<li><a href="/#empresa">Empresa</a></li>
-					<li><a href="/#blog">Blog</a></li>
-					<li><a href="/#contato">Contato</a></li>
-				</ul>
-			</div>
-			<div class="redes-sociais">
-				<ul>
-					<li class="face">
-						<a href="">
-							<img src="<?= $home; ?>/src/images/facebook.svg" title="Facebook" alt="Facebook">
-						</a>
-					</li>
-					<li class="insta">
-						<a href="">
-							<img src="<?= $home; ?>/src/images/instagram.svg" title="Instagram" alt="Instagram">
-						</a>
-					</li>
-					<li class="linkedin">
-						<a href="">
-							<img src="<?= $home; ?>/src/images/linkedin.svg" title="LinkedIn" alt="LinkedIn">
-						</a>
-					</li>
-					<li class="whats">
-						<a href="">
-							<img src="<?= $home; ?>/src/images/whatsapp.svg" title="WhatsApp" alt="WhatsApp">
-						</a>
-					</li>
-				</ul>
-			</div>
-			<div class="menu-mob">
-				<img src="<?= $home; ?>/src/images/bars.svg" alt="Menu mobile" class="icon">
-			</div>
+    <div class="container">
+        <a href="/">
+            <div class="logo">
+                <?php
+                // Supondo que você tenha o ID do post do tipo 'list-menu'
+                $id_post = get_posts(array(
+                    'post_type' => 'list-menu',
+                    'numberposts' => 1
+                ))[0]->ID;
 
-			<div class="content-menu">
-				<div class="close">
-					<img src="<?= $home; ?>/src/images/times.svg?v=1" alt="Fechar">
-				</div>
-				<div class="menu">
-					<ul>
-						<li><a href="/#servicos">Serviços</a></li>
-						<li><a href="/#portfolio">Portfólio</a></li>
-						<li><a href="/#empresa">Empresa</a></li>
-						<li><a href="/#blog">Blog</a></li>
-						<li><a href="/#contato">Contato</a></li>
-					</ul>
-				</div>
-				<div class="redes-sociais">
-					<ul>
-						<li class="face">
-							<a href="">
-								<img src="<?= $home; ?>/src/images/facebook.svg" title="Facebook" alt="Facebook">
-							</a>
-						</li>
-						<li class="insta">
-							<a href="">
-								<img src="<?= $home; ?>/src/images/instagram.svg" title="Instagram" alt="Instagram">
-							</a>
-						</li>
-						<li class="linkedin">
-							<a href="">
-								<img src="<?= $home; ?>/src/images/linkedin.svg" title="LinkedIn" alt="LinkedIn">
-							</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</div>
-	</header>
+                // Recupera o ID do logo a partir dos campos personalizados
+                $itemLogo = get_post_meta($id_post, 'itemLogo', true);
+
+                // Se houver um logo definido, exibe a imagem; caso contrário, exibe uma imagem padrão
+                if ($itemLogo) {
+                    // Recupera a URL do anexo pelo ID
+                    $logo_url = wp_get_attachment_url($itemLogo);
+                    echo '<img src="' . esc_url($logo_url) . '" alt="Logo">';
+                } else {
+                    echo '<img src="' . esc_url($home . '/src/images/logo-3.jpg') . '" alt="Logo Padrão">';
+                }
+                ?>
+            </div>
+        </a>
+        <div class="menu">
+            <ul>
+                <?php
+                // Recuperando os valores dos campos personalizados
+                $itemServicesInativar = get_post_meta($id_post, 'itemServicesInativar', true);
+                $itemPortfolioInativar = get_post_meta($id_post, 'itemPortfolioInativar', true);
+                $itemEmpresaInativar = get_post_meta($id_post, 'itemEmpresaInativar', true);
+                $itemBlogInativar = get_post_meta($id_post, 'itemBlogInativar', true);
+                $itemContatoInativar = get_post_meta($id_post, 'itemContatoInativar', true);
+
+                // Exibindo os itens se não estiverem inativados
+                if ($itemServicesInativar !== '1') {
+                    echo '<li><a href="/#servicos">Serviços</a></li>';
+                }
+                if ($itemPortfolioInativar !== '1') {
+                    echo '<li><a href="/#portfolio">Portfólio</a></li>';
+                }
+                if ($itemEmpresaInativar !== '1') {
+                    echo '<li><a href="/#empresa">Empresa</a></li>';
+                }
+                if ($itemBlogInativar !== '1') {
+                    echo '<li><a href="/#blog">Blog</a></li>';
+                }
+                if ($itemContatoInativar !== '1') {
+                    echo '<li><a href="/#contato">Contato</a></li>';
+                }
+                ?>
+            </ul>
+        </div>
+        <div class="redes-sociais">
+            <ul>
+                <?php
+                // Recuperando os links das redes sociais a partir dos campos personalizados
+                $itemFacebookLink = get_post_meta($id_post, 'itemFacebookLink', true);
+                $itemInstagramLink = get_post_meta($id_post, 'itemInstagramLink', true);
+                $itemLinkedInLink = get_post_meta($id_post, 'itemLinkedInLink', true);
+                $itemWhatsAppLink = get_post_meta($id_post, 'itemWhatsAppLink', true);
+
+								// Verificando se os links das redes sociais estão ativados
+                $itemFacebookLinkInativar = get_post_meta($id_post, 'itemFacebookInativar', true);
+                $itemInstagramLinkInativar = get_post_meta($id_post, 'itemInstagramInativar', true);
+                $itemLinkedInLinkInativar = get_post_meta($id_post, 'itemInativaredInInativar', true);
+                $itemWhatsAppLinkInativar = get_post_meta($id_post, 'itemWhatsAppInativar', true);
+
+                if ($itemFacebookLinkInativar !== '1') {
+                    echo '<li class="face"><a href="' . esc_url($itemFacebookLink) . '"><img src="' . esc_url($home . '/src/images/facebook.svg') . '" title="Facebook" alt="Facebook"></a></li>';
+                }
+                if ($itemInstagramLinkInativar !== '1') {
+                    echo '<li class="insta"><a href="' . esc_url($itemInstagramLink) . '"><img src="' . esc_url($home . '/src/images/instagram.svg') . '" title="Instagram" alt="Instagram"></a></li>';
+                }
+                if ($itemLinkedInLinkInativar !== '1') {
+                    echo '<li class="linkedin"><a href="' . esc_url($itemLinkedInLink) . '"><img src="' . esc_url($home . '/src/images/linkedin.svg') . '" title="LinkedIn" alt="LinkedIn"></a></li>';
+                }
+                if ($itemWhatsAppLinkInativar !== '1') {
+                    echo '<li class="whats"><a href="' . esc_url($itemWhatsAppLink) . '"><img src="' . esc_url($home . '/src/images/whatsapp.svg') . '" title="WhatsApp" alt="WhatsApp"></a></li>';
+                }
+                ?>
+            </ul>
+        </div>
+        <div class="menu-mob">
+            <img src="<?= esc_url($home . '/src/images/bars.svg'); ?>" alt="Menu mobile" class="icon">
+        </div>
+
+        <div class="content-menu">
+            <div class="close">
+                <img src="<?= esc_url($home . '/src/images/times.svg?v=1'); ?>" alt="Fechar">
+            </div>
+            <div class="menu">
+                <ul>
+                    <?php
+                    // Repetindo a lógica dos itens do menu
+                    if ($itemServicesInativar !== '1') {
+                        echo '<li><a href="/#servicos">Serviços</a></li>';
+                    }
+                    if ($itemPortfolioInativar !== '1') {
+                        echo '<li><a href="/#portfolio">Portfólio</a></li>';
+                    }
+                    if ($itemEmpresaInativar !== '1') {
+                        echo '<li><a href="/#empresa">Empresa</a></li>';
+                    }
+                    if ($itemBlogInativar !== '1') {
+                        echo '<li><a href="/#blog">Blog</a></li>';
+                    }
+                    if ($itemContatoInativar !== '1') {
+                        echo '<li><a href="/#contato">Contato</a></li>';
+                    }
+                    ?>
+                </ul>
+            </div>
+            <div class="redes-sociais">
+                <ul>
+                    <?php
+                    // Repetindo a lógica dos links das redes sociais no menu móvel
+                    if ($itemFacebookLinkInativar !== '1') {
+                        echo '<li class="face"><a href="' . esc_url($itemFacebookLink) . '"><img src="' . esc_url($home . '/src/images/facebook.svg') . '" title="Facebook" alt="Facebook"></a></li>';
+                    }
+                    if ($itemInstagramLinkInativar !== '1') {
+                        echo '<li class="insta"><a href="' . esc_url($itemInstagramLink) . '"><img src="' . esc_url($home . '/src/images/instagram.svg') . '" title="Instagram" alt="Instagram"></a></li>';
+                    }
+                    if ($itemLinkedInLinkInativar !== '1') {
+                        echo '<li class="linkedin"><a href="' . esc_url($itemLinkedInLink) . '"><img src="' . esc_url($home . '/src/images/linkedin.svg') . '" title="LinkedIn" alt="LinkedIn"></a></li>';
+                    }
+                    if ($itemWhatsAppLinkInativar !== '1') {
+                        echo '<li class="whats"><a href="' . esc_url($itemWhatsAppLink) . '"><img src="' . esc_url($home . '/src/images/whatsapp.svg') . '" title="WhatsApp" alt="WhatsApp"></a></li>';
+                    }
+                    ?>
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
+
+
+
 
